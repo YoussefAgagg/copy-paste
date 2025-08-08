800b957add44d09f724cf0664bbbf1d1e49e66c9
Youssef Agagg <youssefagagg@gmail.com>
2025-08-08 23:43:17 +0300

    Refactored `Main` constructor to use lambda expressions for cleaner and more concise code.

--- src/main/java/com/github/youssefagagg/Main.java
+++ src/main/java/com/github/youssefagagg/Main.java
@@ -15,30 +15,27 @@ public class Main {
     }
 
     public Main() {
-        EventQueue.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                    try {
-                        UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
-                    } catch (ClassNotFoundException | InstantiationException | IllegalAccessException |
-                             UnsupportedLookAndFeelException ex) {
-                    }
-
-                final JFrame frame = new JFrame("Copy-Paste");
-                c = new CopyPasteFrame();
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.setLayout(new BorderLayout());
-                frame.add(c);
-                frame.pack();
-                frame.setLocationRelativeTo(null);
-                frame.setVisible(true);
-                frame.addWindowListener(new WindowAdapter() {
-                    @Override
-                    public void windowClosing(WindowEvent e) {
-                        // Application will close automatically with EXIT_ON_CLOSE
-                    }
-                });
+        EventQueue.invokeLater(() -> {
+            try {
+                UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+            } catch (ClassNotFoundException | InstantiationException | IllegalAccessException |
+                     UnsupportedLookAndFeelException ignored) {
             }
+
+            final JFrame frame = new JFrame("Copy-Paste");
+            c = new CopyPasteFrame();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setLayout(new BorderLayout());
+            frame.add(c);
+            frame.pack();
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+            frame.addWindowListener(new WindowAdapter() {
+                @Override
+                public void windowClosing(WindowEvent e) {
+                    // Application will close automatically with EXIT_ON_CLOSE
+                }
+            });
         });
     }
 }
